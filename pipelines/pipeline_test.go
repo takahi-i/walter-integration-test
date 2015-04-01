1213a35fbf9da59a07f77d997bf7ac35d1e82c49
takahi-i <takahikoito@gmail.com>
2015-04-01 19:14:01 +0900

    Make first test pass

--- pipelines/pipeline_test.go
+++ pipelines/pipeline_test.go
@@ -17,11 +17,75 @@
 package pipelines
 
 import (
+	"bytes"
+	"io"
+	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/recruit-tech/walter/log"
 )
 
+func execCommand(cmd *exec.Cmd, prefix string) (bool, *string, *string) {
+	out, err := cmd.StdoutPipe()
+	outE, errE := cmd.StderrPipe()
+
+	if err != nil {
+		log.Warnf("[command] %s err: %s", prefix, out)
+		log.Warnf("[command] %s err: %s", prefix, err)
+		return false, nil, nil
+	}
+
+	if errE != nil {
+		log.Warnf("[command] %s err: %s", prefix, outE)
+		log.Warnf("[command] %s err: %s", prefix, errE)
+		return false, nil, nil
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		log.Warnf("[command] %s err: %s", prefix, err)
+		return false, nil, nil
+	}
+	outResult := copyStream(out, prefix)
+	errResult := copyStream(outE, prefix)
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Warnf("[command] %s err: %s", prefix, err)
+		return false, &outResult, &errResult
+	}
+	return true, &outResult, &errResult
+}
+
+func copyStream(reader io.Reader, prefix string) string {
+	var err error
+	var n int
+	var buffer bytes.Buffer
+	tmpBuf := make([]byte, 1024)
+	for {
+		if n, err = reader.Read(tmpBuf); err != nil {
+			break
+		}
+		buffer.Write(tmpBuf[0:n])
+		log.Infof("[command] %s output: %s", prefix, tmpBuf[0:n])
+	}
+	if err == io.EOF {
+		err = nil
+	} else {
+		log.Error("ERROR: " + err.Error())
+	}
+	return buffer.String()
+}
+
+func runCommand(param string) bool {
+	cmd := exec.Command("../bin/walter", "-c", param)
+	cmd.Dir = "."
+	result, _, _ := execCommand(cmd, "exec")
+	return result
+}
+
 func TestReportStageResultWithFullOutput(t *testing.T) {
+	runCommand("pipeline.yml")
 	assert.Equal(t, 2, 2)
 }
